c15e4626c04823561d5b5688ba99df2fc626c2dd
Gu Jiajun <gujiajun@wondersgroup.com>
2017-06-27 15:50:40 +0800

    search bar

---
+++ IndoorMap/Medule/Home/View/SearchBarView.swift
@@ -0,0 +1,80 @@
+//
+//  SearchBarView.swift
+//  IndoorMap
+//
+//  Created by Gu Jiajun on 2017/6/27.
+//  Copyright © 2017年 gjj. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class SearchBarView: UIView, UITextFieldDelegate {
+
+    typealias GotoSearchBlock = () -> ()
+    typealias SearchBlock = (String!) -> ()
+
+    
+    var textField = UITextField()
+    var shouldGotoSearch = true //是否跳转去搜索
+    var gotoSearchBlock: GotoSearchBlock?
+    var searchBlock: SearchBlock?
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = 5
+        self.layer.masksToBounds = true
+        self.backgroundColor = UIColor.white
+        
+        
+        textField.placeholder = "搜索"
+        textField.font = UIFont.systemFont(ofSize: 15)
+        textField.borderStyle = .none
+        textField.delegate = self
+        self.addSubview(textField)
+        textField.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self.snp.top).offset(2)
+            make.bottom.equalTo(self.snp.bottom).offset(-2)
+            make.left.equalTo(self.snp.left).offset(5)
+            make.right.equalTo(self.snp.right).offset(-5)
+        }
+    }
+   
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if shouldGotoSearch {
+            gotoSearchBlock!()
+        } else {
+            return true
+        }
+        
+        return false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if !(textField.text?.isEmpty)! {
+            searchBlock!(textField.text)
+        }
+        return true
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- IndoorMap/Medule/Home/ViewController/HomeViewController.swift
+++ IndoorMap/Medule/Home/ViewController/HomeViewController.swift
@@ -7,17 +7,21 @@
 //
 
 import UIKit
-
+import SnapKit
+import RxSwift
+import RxCocoa
 
 class HomeViewController: BaseViewController, MAMapViewDelegate {
 
     var mapView: MAMapView!
+    var searchBarView = SearchBarView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         initMapView()
+        setupView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,9 +47,25 @@ class HomeViewController: BaseViewController, MAMapViewDelegate {
         mapView.compassOrigin = CGPoint(x: mapView.compassOrigin.x, y: 60)
         mapView.showsScale = true
         mapView.scaleOrigin = CGPoint(x: 5, y: mapView.bounds.size.height-40)
+        //缩放级别（默认3-19，有室内地图时为3-20）
+        mapView.zoomLevel = 15
         self.view.addSubview(mapView!)
     }
     
+    func setupView() {
+        weak var weakSelf = self
+        searchBarView.gotoSearchBlock = {
+            let vc = SearchViewController()
+            weakSelf?.navigationController?.pushViewController(vc, animated: true)
+        }
+        self.view.addSubview(searchBarView)
+        searchBarView.snp.makeConstraints { (make) in
+            make.top.equalTo(self.view.snp.top).offset(25)
+            make.left.equalTo(self.view.snp.left).offset(20)
+            make.right.equalTo(self.view.snp.right).offset(-20)
+            make.height.equalTo(35)
+        }
+    }
 
     /*
     // MARK: - Navigation
